4ade437e26280cad1fc02952fa226709e1a1ec54
Carlos Manoel V A Costa <cavac10@gmail.com>
2021-07-30 11:36:36 -0300

    first commit

---
+++ src/br/com/dio/model/Gato.java
@@ -0,0 +1,93 @@
+package br.com.dio.model;
+
+public class Gato {
+	
+	private String name;
+	private String color;
+	private Integer age;
+	
+	
+	public Gato(String name, String color, Integer age) {		
+		this.name = name;
+		this.color = color;
+		this.age = age;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public String getColor() {
+		return color;
+	}
+
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+
+	public Integer getAge() {
+		return age;
+	}
+
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((age == null) ? 0 : age.hashCode());
+		result = prime * result + ((color == null) ? 0 : color.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Gato other = (Gato) obj;
+		if (age == null) {
+			if (other.age != null)
+				return false;
+		} else if (!age.equals(other.age))
+			return false;
+		if (color == null) {
+			if (other.color != null)
+				return false;
+		} else if (!color.equals(other.color))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Gato [name=" + name + ", color=" + color + ", age=" + age + "]";
+	}
+	
+	
+
+}
